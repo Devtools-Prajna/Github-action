0a190945cf53954d1a416795ab67e1f25547b59f
Prajna Prakash Shetty <prajnap@devtools.in>
2025-06-26 17:10:57 +0530

    Update test_script.py

--- test_script.py
+++ test_script.py
@@ -1,4 +1,4 @@
-from build_program import find_greatest
+from Program1 import find_greatest
 
 print(find_greatest(100, 50, 20))   # Expected: num1 is greater
 print(find_greatest(10, 200, 100))  # Expected: num2 is greatest
