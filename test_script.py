cfad7e6cc56e79ad59fc24bd6a57ed280dde959b
Prajna Prakash Shetty <prajnap@devtools.in>
2025-06-26 16:57:51 +0530

    Create test_script.py

---
+++ test_script.py
@@ -0,0 +1,6 @@
+from build_program import find_greatest
+
+print(find_greatest(100, 50, 20))   # Expected: num1 is greater
+print(find_greatest(10, 200, 100))  # Expected: num2 is greatest
+print(find_greatest(10, 50, 300))   # Expected: num3 is greatest
+print(find_greatest(50, 50, 50))    # Expected: all numbers are equal
