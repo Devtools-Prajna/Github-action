2b885c4b09902dcc5f1b0f8fb79a9616e547e57f
Prajna Prakash Shetty <prajnap@devtools.in>
2025-06-29 21:10:09 +0530

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -1,7 +1,6 @@
-#/bin/sh
+#!/bin/bash
+set -e
 sudo apt update
-sudo apt -y install python3 
-sudo apt -y install python3-pip 
-python3 --version
-echo -e "5\n+\n3" | python3 Project.py
+sudo apt -y install python3
+echo -e "1\n5\n3" | python3 Project.py
 cat Project.py
