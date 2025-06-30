a80f6bf913168cc93197d4ca204dd87b0cabd71d
Prajna Prakash Shetty <prajnap@devtools.in>
2025-06-30 19:49:35 +0530

    Create converter.js

---
+++ unit-converter/converter.js
@@ -0,0 +1,12 @@
+function metersToFeet(meters) {
+  return meters * 3.28084;
+}
+
+function feetToMeters(feet) {
+  return feet / 3.28084;
+}
+
+module.exports = {
+  metersToFeet,
+  feetToMeters
+};
